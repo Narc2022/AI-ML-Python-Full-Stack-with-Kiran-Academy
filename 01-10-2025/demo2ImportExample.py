46bd27c907077208d0b1105304eb3d658e4708f1
sachin <sachinnarofficials@gmail.com>
2025-10-05 17:56:10 +0530

    from demo1 import *

--- 01-10-2025/demo2ImportExample.py
+++ 01-10-2025/demo2ImportExample.py
@@ -2,7 +2,17 @@ age = 26
 course = "Data Science"
 print(age)
 print(course)
-import demo1
 
-print(demo1.name)
-print(demo1.city)
+# import demo1
+
+# print(demo1.name)
+
+# from demo1 import city, name
+
+# print(city)
+# print(name)
+
+from demo1 import *
+
+print(name)
+print(age)
